652b9c863f7dddf775f9b2cae520859c0f269db1
altendorfme <altendorfme@gmail.com>
2021-02-17 15:04:59 -0300

    update amazon wrapper

--- am.js
+++ am.js
@@ -9,24 +9,35 @@ module.exports = function (url) {
       });
 
       const page = await browser.newPage();
-
+      const numberPattern = /\d+/g;
+      
       await page.setViewport({width: 1366, height: 768});
 
       await page.goto(url, {
         waitUntil: ['load', 'domcontentloaded','networkidle0','networkidle2'],
       });
 
-      await page.mouse.move(640, 340);
+      const doses = [];
 
-      await page.waitForTimeout(2000);
-
-      const data = await page.evaluate(
-        () => document.querySelector("html").outerHTML
+      await page.mouse.move(560, 315);
+      await page.waitForXPath('//body//div[contains(@class, "tab-tooltip")]');
+      const data1 = await page.evaluate(
+        () => document.querySelectorAll('.tab-ubertipTooltip span')[0].innerText
       );
-      
+      var split_string1 = data1.split(":");
+      doses.push(split_string1[1].match(numberPattern).join(''));
+  
+      await page.mouse.move(560, 380);
+      await page.waitForXPath('//body//div[contains(@class, "tab-tooltip")]');
+      const data2 = await page.evaluate(
+        () => document.querySelectorAll('.tab-ubertipTooltip span')[0].innerText
+      );
+      var split_string2 = data2.split(":");
+      doses.push(split_string2[1].match(numberPattern).join(''));
+
       await browser.close();
 
-      resolve(data);
+      resolve(JSON.stringify(doses));
     })();
   });
 };
